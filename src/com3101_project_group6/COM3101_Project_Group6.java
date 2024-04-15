1e2eb6c436f168ee838bd57bc215d0e2b5b2125b
Cyrus Lau <cyruslau.work@gmail.com>
2024-04-15 15:29:53 +0800

    init

---
+++ src/com3101_project_group6/COM3101_Project_Group6.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com3101_project_group6;
+
+/**
+ *
+ * @author cyrus
+ */
+public class COM3101_Project_Group6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
