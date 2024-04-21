ae6d4557371c0d69201293a8c3f8c37420f489c5
WiSmYlOveHat <93336667+WiSmYlOveHat@users.noreply.github.com>
2024-04-21 23:28:00 +0800

    Pre:caretaker and memento update

---
+++ src/com3101_project_group6/CareTaker.java
@@ -0,0 +1,22 @@
+package com3101_project_group6;
+
+import java.util.*;
+
+class CareTaker {
+
+    private ArrayList<Memento> undoList = new ArrayList<>();
+
+    public void addMemento(Memento m) {
+        undoList.add(0, m);
+    }
+
+    public Memento removeMemento() {
+
+        if (!undoList.isEmpty()) {            
+            Memento m = undoList.remove(0);
+            return m;
+        } else {
+            return null;
+        }
+    }    
+}
---
+++ src/com3101_project_group6/Memento.java
@@ -0,0 +1,24 @@
+package com3101_project_group6;
+
+import java.util.*;
+
+public class Memento {
+    int playerId;
+    int balance;
+    String name;
+    int token;
+    int stelp;
+    boolean isPlayer;
+    public Memento(int playerId, int balance, String name, int token, int stelp, boolean isPlayer) {
+        this.playerId = playerId;
+        this.balance = balance;
+        this.name = name;
+        this.token = token;
+        this.stelp = stelp;
+        this.isPlayer = isPlayer;
+    }
+    
+    
+    
+    
+}
