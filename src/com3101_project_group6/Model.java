1a29745f0637ec5e0c24ca441eae7073fa64d081
Peter <wearefriend3456@gmail.com>
2024-04-22 19:55:37 +0800

    object added

--- src/com3101_project_group6/Model.java
+++ src/com3101_project_group6/Model.java
@@ -4,6 +4,8 @@
  */
 package com3101_project_group6;
 
+
+import java.util.*;
 /**
  *
  * @author cyrus
@@ -12,6 +14,7 @@ public class Model {
 
     private static Model _instance;
     private Controller controller;
+    List<Memento> playerList = new ArrayList<>();
 
     private Model() {
         // Private constructor to prevent direct instantiation
@@ -26,5 +29,53 @@ public class Model {
 
     public void setController(Controller c) {
         this.controller = c;
+    }
+    
+    void setPlayer(int amount){
+        // player number (1-4)
+        if(amount <=3 && amount >= 0){
+            // do something
+        }else{
+            //show message: not correct number of player,please config again
+        }
+        
+    }
+    void setBot(int amount){
+        // limit the number of bot (0-3)
+        if(amount <=3 && amount >= 0){
+            // do something
+        }else{
+            //show message: not correct number of Bot,please config again
+        }
+    }
+    void startGame(){
+        
+    } 
+    void rollDice(){
+        
+    }
+    void buyProperty(){
+        
+    }
+    void payRentforProperty(){
+        
+    }
+    void payRentforBusStop(){
+        
+    }
+    void payTax(){
+        
+    }
+    void tradeProperty(){
+        
+    }
+    void endTrun(){
+        
+    }
+    void endGame(){
+        
+    }
+    void jailed(){
+        
     }
 }
